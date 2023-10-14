71e7212947d97db2b488fa760344aafb4fb9c122
tluuchau1 <tluuchau1@gmail.com>
2023-10-14 13:18:26 +0700

    album and post

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,33 +1,44 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-   author: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-   },
-
-   vacation: {
-      type: String,
-      required: true,
-   },
-
-   content: {
-      type: String,
-      required: true,
-   },
-
-   images: {
-      type: Array,
-      required: true,
-   },
-
-   createAt: {
-      type: String,
-   },
-
-   updateAt: {
-      type: String,
-   },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  vacation: {
+    type: String,
+    default: "kỳ nghỉ hiện tại",
+  },
+  milestones: [
+    {
+      time: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  content: {
+    type: String,
+    required: true,
+  },
+  mediaUrls: {
+    type: [String],
+    default: [],
+  },
+  createAt: {
+    type: Date,
+    default: Date.Now,
+  },
+  updateAt: {
+    type: Date,
+    default: Date.Now,
+  },
 });
 
 const PostModel = mongoose.model("Posts", postSchema);
+
+export default PostModel;
