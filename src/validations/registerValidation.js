626a4b026aa37c660e4b1a906b73183d4570d5c2
Xuyen <hoangxuyenpham30@gmail.com>
2023-10-14 17:00:43 +0700

    update-user-schema

--- src/validations/registerValidation.js
+++ src/validations/registerValidation.js
@@ -10,14 +10,14 @@ const registerSchema = Yup.object().shape({
       .required("Số điện thoại hông được để trống"),
 
    userName: Yup.string()
-      .min("Tối thiểu 6 kí tự")
-      .max("Tối đa 20 kí tự")
+      .min(6, "Tối thiểu 6 kí tự")
+      .max(20, "Tối đa 20 kí tự")
       .required("Username không được để trống"),
 
    password: Yup.string()
       .required("Vui lòng nhập mật khẩu của bạn")
-      .min("Mật khẩu tối thiểu 6 kí tự")
-      .max("Mật khẩu tối đa 10 kí tự")
+      .min(6, "Mật khẩu tối thiểu 6 kí tự")
+      .max(10, "Mật khẩu tối đa 10 kí tự")
       .matches(
          /^[a-zA-Z0-9!@#$%^&*()_+=\\[\]{}|;:'",.<>/?`~]*$/,
          "Mật khẩu gồm chữ in hoa, chữ thường, số và kí tự đặc biệt"
