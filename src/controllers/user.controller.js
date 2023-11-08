dcf1f05ffd6bc64ef1f36d29dc567b3b64052269
Xuyen <hoangxuyenpham30@gmail.com>
2023-11-09 04:13:13 +0700

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -44,6 +44,9 @@ const UserController = {
       const avatarUrl = await uploadImage(file);
 
       const updateUser = await UserModel.findByIdAndUpdate(userId, { avatar: avatarUrl }, { new: true }).select("-password");
+      res.status(200).json({
+         data: updateUser,
+      });
    }),
 
    updatePassword: asyncHandler(async (req, res) => {
